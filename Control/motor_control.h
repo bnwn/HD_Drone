aaa9c4e01e68ba66348a0c9f7e39ac4aa407ff78
bnwn <bnwn_bnwn@126.com>
2017-08-13 03:52:29 -0700

    todo test balance

---
+++ Control/motor_control.h
@@ -0,0 +1,25 @@
+#ifndef __MOTOR_CONTROL
+#define __MOTOR_CONTROL
+
+#include "../Driver/motor.h"
+#include "../AHRS/ahrs.h"
+#include "../Algorithm/Algorithm_math/mymath.h"
+#include "../Algorithm/Algorithm_math/Algorithm_math.h"
+
+typedef struct {
+    float roll;
+    float pitch;
+    float yaw;
+    float throttle;
+}Thrust;
+
+void motors_output(void);
+void throttle_control(float _dt);
+void set_motor_roll(float _thrust);
+void set_motor_pitch(float _thrust);
+void set_motor_yaw(float _thrust);
+void set_motor_throttle(float _thrust);
+
+extern Thrust thrust;
+extern float target_throttle;
+#endif
